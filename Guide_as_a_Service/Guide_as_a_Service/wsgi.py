bafb578794334fbe7649cd98d9f8a9411399a004
Priyanka <patilpriyanka001@gmail.com>
2018-05-23 10:59:45 -0700

    3rd commit

---
+++ Guide_as_a_Service/Guide_as_a_Service/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Guide_as_a_Service project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Guide_as_a_Service.settings")
+
+application = get_wsgi_application()
